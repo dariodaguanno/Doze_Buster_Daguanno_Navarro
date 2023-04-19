6226b1666b48ad74aea198c4a6bfd0154759776f
dariodaguanno <tul11082@temple.edu>
2023-04-19 14:19:33 -0400

    If proximity value is outside threshold then goes back in the screen will return to normal

--- app/src/main/java/com/example/dozebustersensor/MainActivity.java
+++ app/src/main/java/com/example/dozebustersensor/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private static final float PROXIMITY_THRESHOLD_MIN = 3.0f; // minimum proximity threshold
     private static final float PROXIMITY_THRESHOLD_MAX = 5.0f; // maximum proximity threshold
 
+    private boolean isOutsideThreshold = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,27 +44,31 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
+                    float proximityValue = sensorEvent.values[0];// get the sensor value
 
-                float proximityValue = sensorEvent.values[0];// get the sensor value
+                    if (proximityValue >= PROXIMITY_THRESHOLD_MIN && proximityValue <= PROXIMITY_THRESHOLD_MAX) {
+                        if(isOutsideThreshold) {
+                            isOutsideThreshold = false;
+                            ImageView imageview = (ImageView) findViewById(R.id.proximityImage);
+                            imageview.setVisibility(imageview.VISIBLE);
+                            getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                        }
 
-                if (proximityValue >= PROXIMITY_THRESHOLD_MIN && proximityValue <= PROXIMITY_THRESHOLD_MAX){
-                    ImageView imageview = (ImageView) findViewById(R.id.proximityImage);
-                    imageview.setVisibility(imageview.VISIBLE);
-                    getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                    } else {
+                        if(!isOutsideThreshold) {
+                            isOutsideThreshold = true;
+                            ImageView imageview = (ImageView) findViewById(R.id.proximityImage);
+                            imageview.setVisibility(imageview.INVISIBLE);
+                            getWindow().getDecorView().setBackgroundColor(Color.RED);
+                            TextView textview = (TextView) findViewById(R.id.proximityText);
+                            textview.setText("Come back and study!");
 
+                            //play sound
+                            MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.mixkit_double_beep_tone_alert_2868);
+                            mediaPlayer.start();
+                        }
+                    }
 
-
-                }else {
-                    ImageView imageview = (ImageView) findViewById(R.id.proximityImage);
-                    imageview.setVisibility(imageview.INVISIBLE);
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                    TextView textview = (TextView)  findViewById(R.id.proximityText);
-                    textview.setText("Come back and study!");
-
-                    //play sound
-                    MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.mixkit_double_beep_tone_alert_2868);
-                    mediaPlayer.start();
-                }
             }//uilu789
 
             @Override
